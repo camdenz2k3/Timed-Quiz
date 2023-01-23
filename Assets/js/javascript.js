42ed6a4d014dc755f04303e4f4a1a75fed57c0fa
Snow Cactus <camdenz2k3@gmail.com>
2023-01-23 16:44:07 -0600

    html for front end quiz and added some folders to be more organized

---
+++ Assets/js/javascript.js
@@ -0,0 +1,15 @@
+// variables
+
+var timer = document.getElementById("timer");
+var timeLeft = document.getElementById("timeLeft");
+var timesUp = document.getElementById("timesUp");
+
+var startDiv = document.getElementById("start");
+var startQuizBtn = document.getElementById("start-quiz-button");
+
+var questionDiv = document.getElementById("questionDiv");
+var questionTitle = document.getElementById("questionTitle");
+var choiceA = document.getElementById("btn0");
+var choiceB = document.getElementById("btn1");
+var choiceC = document.getElementById("btn2");
+var choiceD = document.getElementById("btn3");
